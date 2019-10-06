feef436660d90a60a65b65d2a18bab4b0022a514
Lars Kiesow <lkiesow@uos.de>
2019-10-06 12:25:00 +0200

    Fix Setup
    Thsi patch fixes a problem where some files where missing when the
    library was installed via pip.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='sentinel5dl',
-    version='0.1',
+    version='0.2',
     description='Sentinel-5p Downloader',
     author='Emissions API Developers',
     license='MIT',
     url='https://github.com/emissions-api/sentinel5dl',
     packages=find_packages(),
-    install_requires=open('requirements.txt').read().splitlines(),
-    long_description=open('readme.md').read(),
+    install_requires=['pycurl>=7.43.0'],
+    long_description='This library provides easy access to data from the '
+                     'European Space Agency\'s Sentinel 5P sattellite.',
     entry_points={
         'console_scripts': [
             'sentinel5dl = sentinel5dl.__main__:main'

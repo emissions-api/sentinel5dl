22312086f58ce5b6b1dacf1341bdb6d0c1b7d61e
Lars Kiesow <lkiesow@uos.de>
2019-11-27 01:50:37 +0100

    Release 0.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename):
 
 setup(
     name='sentinel5dl',
-    version='0.5',
+    version='0.6',
     description='Sentinel-5p Downloader',
     author='Emissions API Developers',
     license='MIT',

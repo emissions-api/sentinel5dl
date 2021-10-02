67bf50bf8ddc48aa0177df534b2084a733d8b8c9
Lars Kiesow <lkiesow@uos.de>
2021-10-02 17:34:33 +0200

    Release 1.1
    List of important changes. For a comprehensive list, take a look at the commit history.
    
    - [Additional Executable Tests
      ](https://github.com/emissions-api/sentinel5dl/pull/62)
    - [Introduce Bandit Security Linter
      ](https://github.com/emissions-api/sentinel5dl/pull/65)
    - [Fix return values from retried HTTP requests
      ](https://github.com/emissions-api/sentinel5dl/pull/76)
    - [Fix some error messages
      ](https://github.com/emissions-api/sentinel5dl/pull/77)
    - [Fix renaming temporary files causing errors on Windows
      ](https://github.com/emissions-api/sentinel5dl/pull/78)

--- setup.py
+++ setup.py
@@ -11,20 +11,21 @@ def read(filename):
 
 setup(
     name='sentinel5dl',
-    version='1.0',
+    version='1.1',
     description='Sentinel-5p Downloader',
     author='Emissions API Developers',
     license='MIT',
     url='https://github.com/emissions-api/sentinel5dl',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: GIS',
     ],
